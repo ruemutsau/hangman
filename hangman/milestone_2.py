c1766410026bc07b2964c47bd8178caedee365e6
ruemutsau <149763297+ruemutsau@users.noreply.github.com>
2023-11-05 15:55:30 +0000

    Update milestone_2.py

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,2 +1,6 @@
 words = ['apple', 'banana', 'cherry', 'date', 'elderberry']
 print (words)
+import random
+words = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+random_word = random.choice(words)
+print(random_word)
