ff368705e2b287e39e29cf8bb1c3e1ed040454dd
ruemutsau <149763297+ruemutsau@users.noreply.github.com>
2023-11-05 16:47:25 +0000

    Update milestone_2.py

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -3,3 +3,5 @@ words = ['apple', 'banana', 'cherry', 'date', 'elderberry']
 random_word = random.choice(words)
 print (words)
 print(random_word)
+letter = input("Please enter a single letter: ")
+print("You entered the letter:", letter)
